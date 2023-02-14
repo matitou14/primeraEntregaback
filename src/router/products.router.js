3fcda83bd2f35e9372e256ed621dfe8fdf569fa1
Matias Rodriguez <mat.rodri@gmail.com>
2023-02-14 15:57:42 -0300

    Ft testing prod/post

--- src/router/products.router.js
+++ src/router/products.router.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const productRouter = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+const prodfile = path.join(__dirname, '../data/products.json');
 
 const products = []
 
@@ -15,9 +19,19 @@ productRouter.get('/:pid', (req, res) => {
     } else {
         res.status(404).json({message:`Product with ID ${pid} not found` });
     }
-  });
+});
+
+  productRouter.post('/', (req, res) => {
+    const { title, description, code, price, status, stock, category, thumbnails } = req.body;
+
+  let products = JSON.parse(fs.readFileSync(prodfile));    
+  const newId = Math.max(...products.map(p => p.id)) + 1;
+    const newProduct = { id: newId, title, description, code, price, status, stock, category, thumbnails };
+    products.push(newProduct);
+    fs.writeFileSync(prodfile, JSON.stringify(products, null, 2));
+    res.status(201).json(newProduct);
+
+});
 
 
-
-  
 module.exports = productRouter;
