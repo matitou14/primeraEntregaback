9894e648c42e950f988bab0b648d955e01ce4cc9
Matias Rodriguez <mat.rodri@gmail.com>
2023-02-14 15:29:22 -0300

    Ft testing get-getPid ok

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
 
-const productsRouter = require('./router/products.router')
-const cartsRouter = require ('./router/cart.router')
+const productRouter = require('./router/products.router')
 
 
 
@@ -12,8 +11,8 @@ const cartsRouter = require ('./router/cart.router')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api/products', productsRouter);
-app.use('/api/carts', cartsRouter );
+app.use('/api/products', productRouter);
+// app.use('/api/carts', cartsRouter );
 
 
 app.listen(8080 , () => { console.log('Server is running on port 8080') })
--- src/router/products.router.js
+++ src/router/products.router.js
@@ -1,98 +1,23 @@
 const express = require('express');
-const router = express.Router();
-const fs = require('fs');
+const productRouter = express.Router();
 
-let products = [];
-let productId = 1;
+const products = []
 
-router.get('/', (req, res) => {
-     fs.writeFile('./products.json', JSON.stringify(products), (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error al escribir en el archivo JSON');
-      } else {
-        res.send({products});
-      }
-    });
-  });
-  
-  // Ruta para obtener un producto por su ID
-  router.get('/:pid', (req, res) => {
-    const productId = req.params.id;
-     fs.writeFile('./products.json', JSON.stringify(products), (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error al escribir en el archivo JSON');
-      } else {
-        res.send({productId});
-      }
-    });
-  });
 
-  router.post('/', express.json(), (req, res) =>  {
-  const product = {
-    id: productId++,
-    title: req.body.title,
-    description: req.body.description,
-    code: req.body.code,
-    price: req.body.price,
-    status: req.body.status,
-    stocks: req.body.stocks,
-    category: req.body.category,
-    thumbnails: req.body.thumbnails
-  };
-    products.push(product);
-    fs.writeFile('./products.json', JSON.stringify(products), (err) => {  
-        if (err) {
-            console.error(err);
-            res.status(500).send('Error al escribir en el archivo JSON');
-        } else {
-            res.send({products});
-            res.status(201).send('Producto creado con éxito');
-        }
-    })
-    });
+productRouter.get('/', (req, res) => {
+  res.json(products);
+});
 
-    router.put('/:pid',(req, res) =>{
-    const productId = req.params.id;
-    const product = products.find(p => p.id === productId);
-    if (product) {  
-        product.title = req.body.title;
-        product.description = req.body.description;
-        product.code = req.body.code;
-        product.price = req.body.price;
-        product.status = req.body.status;
-        product.stocks = req.body.stocks;
-        product.category = req.body.category;
-        product.thumbnails = req.body.thumbnails;
-        fs.writeFile('./products.json', JSON.stringify(products), (err) => {
-            if (err) {
-                console.error(err);
-                res.status(500).send('Error al escribir en el archivo JSON');
-            } else {
-                res.send({products});
-                res.status(201).send('Producto actualizado con éxito');
-            }
-        })
-      }
-    });
-    
-    router.delete('/:pid', (req, res) => {
-    const productId = req.params.id;
-    const product = products.find(p => p.id === productId);
-    if (product) {
-        products = products.filter(p => p.id !== productId);
-        fs.writeFile('./products.json', JSON.stringify(products), (err) => {
-            if (err) {
-                console.error(err);
-                res.status(500).send('Error al escribir en el archivo JSON');
-            } else {
-                res.send({products});
-                res.status(201).send('Producto eliminado con éxito');
-            }
-        })
+productRouter.get('/:pid', (req, res) => {
+  const { pid } = req.params;
+    const product = products.find(p => p.id === parseInt (pid));
+    if (product) { res.json(product);
+    } else {
+        res.status(404).json({message:`Product with ID ${pid} not found` });
     }
-    });
-    
-        
-  module.exports = router
+  });
+
+
+
+  
+module.exports = productRouter;
