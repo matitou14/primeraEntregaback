630c1fe1118ccc758f2665add4097c80383cb37a
Matias Rodriguez <mat.rodri@gmail.com>
2023-02-15 11:00:01 -0300

    Ft testing cart/post/pid

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const cartRouter = require('./router/cart.router')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/products', productRouter);
-app.use('/api/cart', cartRouter );
+app.use('/api/carts', cartRouter );
 
 
 app.listen(8080 , () => { console.log('Server is running on port 8080') })
